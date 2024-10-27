0c296b05bbef207c4f602ad547f620d157c7fd9a
Stuart Geipel <stuartgeipel@gmail.com>
2024-10-26 23:01:25 -0400

    add benchmark

--- src/games/drawbox/main.cpp
+++ src/games/drawbox/main.cpp
@@ -26,6 +26,7 @@ int main() {
 
     scr.setEnableVblankNmi(true);
 
+    int8_t count = 0;
     while (true) {
         bcr.resetIrq();
         waitForInterrupt(); // wait for next interrupt (vblank nmi)
@@ -35,9 +36,21 @@ int main() {
         graphics::clearScreen(0b001'00'100);
         waitForInterrupt();
 
+        if (++count == 10) {
+            count = 0;
+        }
+        for (int8_t x = 0; x < 4; x++) {
+            for (int8_t y = 0; y < 2; y++) {
+                int8_t dx = x * 20 + count, dy = y * 20;
+                graphics::fillTriangle(
+                    {(int8_t) (dx + 10), (int8_t) (dy + 10)},
+                    {(int8_t) (dx + 20), (int8_t) (dy + 25)},
+                    {(int8_t) (dx + 30), (int8_t) (dy + 10)}, ~0b000'01'110);
+            }
 
+        }
 
-        graphics::fillTriangle({10,10}, {80,40}, {50,100}, ~0b000'01'110);
-        graphics::fillTriangle({110,110}, {80,40}, {50,100}, ~0b000'11'100);
+        // graphics::fillTriangle({10,10}, {80,40}, {50,100}, ~0b000'01'110);
+        // graphics::fillTriangle({110,110}, {80,40}, {50,100}, ~0b000'11'100);
     }
 }
